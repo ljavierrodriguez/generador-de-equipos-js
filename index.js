b50b8bf8dae7917a152dcd93327c36c2fa76d5e7
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2021-10-14 00:59:07 +0000

    Generador de Equipos de Alumnos Random

---
+++ index.js
@@ -0,0 +1,42 @@
+let alumnos = ["eliana", "edisson", "kristian", "james", "jairo", "julian", "frandy", "jorge", "gustavo", "erick", "jesus", "luis", "hector", "patricia"];
+
+const generarEquipos = (alumnos = [], nroEquipos = 0) => {
+    let equipos = [];
+    let integrantes = alumnos.length / nroEquipos;
+    if (alumnos.length % nroEquipos !== 0) console.log(`No puede generar ${nroEquipos} con ${alumnos.length} alumnos`);
+
+    for (let i = 0; i < nroEquipos; i++) {
+        console.log(`Armando equipo nro ${i + 1}`);
+        let miembros = {}
+        for (let j = 0; j < integrantes; j++) {
+            let rn = Math.floor(Math.random() * alumnos.length);
+            if (equipos.length > 0) {
+                let encontrado = equipos.find((equipo) => equipo.hasOwnProperty(alumnos[rn]))
+                if (encontrado) {
+                    j--;
+                } else {
+                    if(miembros.hasOwnProperty(alumnos[rn])){
+                        j--;
+                    }else{
+                        miembros[alumnos[rn]] = 'activo';
+                    }
+                }
+            } else {
+                if(miembros.hasOwnProperty(alumnos[rn])){
+                    j--;
+                }else{
+                    miembros[alumnos[rn]] = 'activo';
+                }
+            }
+        }
+        equipos.push(miembros);
+    }
+
+    if (equipos.length > 0){}
+        equipos.forEach((equipo, index) => {
+            console.log(`Equipo nro ${index + 1}`);
+            console.log(equipo)
+        })
+}
+
+generarEquipos(alumnos, 7);
